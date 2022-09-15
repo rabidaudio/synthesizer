3aedf15c2759d7a830dae5413f7b8c3a4f13707d
Charles Julian Knight <charles@rabidaudio.com>
2022-09-14 23:57:54 -0400

    convert pot to rotary switch

--- clock/src/Knob.h
+++ clock/src/Knob.h
@@ -1,99 +1,49 @@
 #pragma once
 #include <Arduino.h>
 
-uint16_t delta(uint16_t a, uint16_t b)
-{
-  if (a > b)
-  {
-    return a - b;
-  }
-  return b - a;
-}
-
-// Read the value of a potentiometer and
-// report back it's value after smoothing and
-// filtering the signal. Implements hysteresis
-// causing the value to be constant unless the knob
-// is moving.
-template <int LP_SAMPLES>
+template <size_t STEPS, size_t LP_SAMPLES>
 class Knob
 {
-private:
-  uint8_t _pin;
-  uint8_t _hysteresis;
-  uint32_t _settle_time_ms;
-  uint16_t _value;
-  bool _moving = false;
-  uint32_t _moving_at = 0;
-  size_t _m_idx;
-  uint16_t _measurements[LP_SAMPLES];
+  private:
+    uint8_t _pin;
+    uint16_t _measurements[LP_SAMPLES];
+    size_t _measurementsIdx = 0;
+    uint16_t _bounds[STEPS + 1];
 
-public:
-  void begin(uint8_t pin, uint16_t hysteresis, uint32_t settle_time_ms)
-  {
-    _pin = pin;
-    _hysteresis = hysteresis;
-    _settle_time_ms = settle_time_ms;
-    _m_idx = 0;
-    // seed filter
-    _moving = true;
-    _moving_at = millis();
-    for (size_t i = 0; i < LP_SAMPLES; i++)
-    {
-      _value = read();
-      delay(settle_time_ms / LP_SAMPLES);
-    }
-    _moving = false;
-  }
+  public:
+    void begin(uint8_t pin) {
+      _pin = pin;
 
-  bool isMoving()
-  {
-    return _moving;
-  }
-
-  uint16_t read()
-  {
-    uint16_t value = analogRead(_pin);
-    _measurements[_m_idx] = value;
-    _m_idx = (_m_idx + 1) % LP_SAMPLES;
-    uint32_t sum = 0;
-    uint16_t max_delta = 0;
-    for (size_t i = 0; i < LP_SAMPLES; i++)
-    {
-      sum += _measurements[i];
-      uint16_t step_delta = delta(_measurements[i], value);
-      if (step_delta > max_delta)
-        max_delta = step_delta;
-    }
-    uint16_t new_value = sum / LP_SAMPLES;
-    if (!_moving && delta(_value, new_value) > _hysteresis)
-    {
-      _moving = true;
-      _moving_at = millis();
-    }
-    else if (_moving)
-    {
-      if (max_delta > _hysteresis)
-      {
-        // still moving
-        _moving_at = millis();
+      // populate LP filter
+      for (size_t i = 0; i < LP_SAMPLES; i++) {
+        _measurements[i] = analogRead(_pin);
+        delay(10);
       }
-      else if ((millis() - _moving_at) >= _settle_time_ms)
-      {
-        _moving = false;
+      _measurementsIdx = 0;
+      
+      // populate cutoffs
+      float step = 1024 / (float) (STEPS - 1);
+      float v = step / 2;
+      for (size_t j = 0; j < STEPS; j++) {
+        _bounds[j] = (uint16_t) round(v);
+        v += step;
       }
+      _bounds[STEPS] = 1024;
     }
 
-    if (_moving)
-    {
-      // always show the most up-to-date value
-      _value = new_value;
-      return new_value;
+    int8_t readChanges() {
+      _measurements[_measurementsIdx] = analogRead(_pin);
+      _measurementsIdx = (_measurementsIdx + 1) % LP_SAMPLES;
+      uint16_t avg = 0;
+      for (size_t i = 0; i < LP_SAMPLES; i++) {
+        avg += _measurements[i];
+      }
+      avg = avg / LP_SAMPLES;
+      for (uint8_t j = 0; j < STEPS; j++) {
+        if (_bounds[j] >= avg) {
+          return j;
+        }
+      }
+      return STEPS;
     }
-    else
-    {
-      // continue to return the saved value
-      return _value;
-    }
-  }
 };
--- clock/src/main.cpp
+++ clock/src/main.cpp
@@ -11,61 +11,67 @@
 #define A_BUTTON_PIN 7
 #define B_BUTTON_PIN 8
 
-Display display;
-Knob<8> knob;
-Button aButton;
-Button bButton;
-TapTempo tapTempo;
+// Display display;
+Knob<12, 8> knob;
+// Button aButton;
+// Button bButton;
+// TapTempo tapTempo;
 
 void setup()
 {
+  delay(1000);
   Serial.begin(9600);
-  Timer.begin(CLOCK_PIN, SUBDIV_PIN);
-  Timer.setSubdivisions(2);
-  Timer.setBPM(120);
-  display.begin();
-  knob.begin(KNOB_PIN, 16, 500);
-  aButton.begin(A_BUTTON_PIN, 100);
-  bButton.begin(B_BUTTON_PIN, 100);
+  // Timer.begin(CLOCK_PIN, SUBDIV_PIN);
+  // Timer.setSubdivisions(2);
+  // Timer.setBPM(120);
+  // display.begin();
+  knob.begin(KNOB_PIN);
+  // aButton.begin(A_BUTTON_PIN, 100);
+  // bButton.begin(B_BUTTON_PIN, 100);
 }
 
 void loop()
 {
-  uint16_t tapBpm = tapTempo.tick(aButton.isPressed());
-  uint16_t knobValue = knob.read();
 
-  if (aButton.isPressed() && bButton.isPressed())
-  {
-    Timer.reset();
-    tapTempo.cancel();
-    display.displayReset();
-  }
-  else if (aButton.isPressed())
-  {
-    if (tapTempo.isActive())
-    {
-      Timer.setBPM(tapBpm);
-      display.displayNumber(tapBpm, 0x08 | (Timer.clockOn() ? 0x01 : 0x00) | (Timer.subdivisionOn() ? 0x02 : 0x00));
-    }
-    else
-    {
-      uint8_t subdivisions = (uint8_t)((knobValue / 64) + 1);
-      Timer.setSubdivisions(subdivisions);
-      display.displayNumber(subdivisions, 0x02);
-    }
-  }
-  else if (bButton.isPressed())
-  {
-    int8_t swing = (int8_t)(((int16_t)knobValue / 4) - 128);
-    Timer.setSwing(swing);
-    display.displayNumber(swing, 0x04);
-  }
-  else
-  {
-    uint16_t bpm = (knobValue / 4) + 32; /* 32 to 287 */
-    Timer.setBPM(bpm);
-    display.displayNumber(bpm, (tapTempo.isActive() ? 0x08 : 0) | (Timer.clockOn() ? 0x01 : 0x00) | (Timer.subdivisionOn() ? 0x02 : 0x00));
-  }
+   uint8_t v = knob.readChanges();
+   Serial.println(v);
+   delay(10);
 
-  delay(10);
+  // uint16_t tapBpm = tapTempo.tick(aButton.isPressed());
+  // uint16_t knobValue = knob.read();
+
+  // if (aButton.isPressed() && bButton.isPressed())
+  // {
+  //   Timer.reset();
+  //   tapTempo.cancel();
+  //   display.displayReset();
+  // }
+  // else if (aButton.isPressed())
+  // {
+  //   if (tapTempo.isActive())
+  //   {
+  //     Timer.setBPM(tapBpm);
+  //     display.displayNumber(tapBpm, 0x08 | (Timer.clockOn() ? 0x01 : 0x00) | (Timer.subdivisionOn() ? 0x02 : 0x00));
+  //   }
+  //   else
+  //   {
+  //     uint8_t subdivisions = (uint8_t)((knobValue / 64) + 1);
+  //     Timer.setSubdivisions(subdivisions);
+  //     display.displayNumber(subdivisions, 0x02);
+  //   }
+  // }
+  // else if (bButton.isPressed())
+  // {
+  //   int8_t swing = (int8_t)(((int16_t)knobValue / 4) - 128);
+  //   Timer.setSwing(swing);
+  //   display.displayNumber(swing, 0x04);
+  // }
+  // else
+  // {
+  //   uint16_t bpm = (knobValue / 4) + 32; /* 32 to 287 */
+  //   Timer.setBPM(bpm);
+  //   display.displayNumber(bpm, (tapTempo.isActive() ? 0x08 : 0) | (Timer.clockOn() ? 0x01 : 0x00) | (Timer.subdivisionOn() ? 0x02 : 0x00));
+  // }
+
+  // delay(10);
 }
